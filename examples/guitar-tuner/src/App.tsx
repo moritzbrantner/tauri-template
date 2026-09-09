0c12d588d5d0ce4a4246d39992a037e44e11e7f0
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-09 12:08:48 +0200

    Handle tuner cleanup promise

--- examples/guitar-tuner/src/App.tsx
+++ examples/guitar-tuner/src/App.tsx
@@ -33,7 +33,7 @@ export default function App() {
     disposedRef.current = false;
     return () => {
       disposedRef.current = true;
-      void sessionRef.current?.stop();
+      sessionRef.current?.stop().catch(() => undefined);
       sessionRef.current = null;
     };
   }, []);
